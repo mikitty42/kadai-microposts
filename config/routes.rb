ba870d7f7797106a4c83baa83bb128f0286e3135
EC2 Default User <ec2-user@ip-172-31-14-26.us-east-2.compute.internal>
2018-08-04 06:00:17 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to:'sessions#destroy'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'toppagees#index'
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:index, :show, :new, :create]
+
+
 end
